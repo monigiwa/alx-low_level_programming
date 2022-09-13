e4992498228374f01284050f9f0d32d31b1a2c39
Giwa <giwatitilope899@gmail.com>
2022-09-13 07:26:51 -0700

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - prints the alphabet in lower case
+ *
+ * Description: prints the alphabet in lower case
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	_putchar ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		_putchar(ch);
+	}
+	return (0);
+}
