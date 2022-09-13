fc3217097a22b33447c554aac93e7a231c72362b
Giwa <giwatitilope899@gmail.com>
2022-09-13 12:27:54 -0700

    task 5&6

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,22 +1,27 @@
 #include "main.h"
 
 /**
- * _islower - checks for lowercase
- * @c: c is an ascii character
+ * print_sign - prints the sign of a number
+ * @n: n is an integer
  *
- * Return: 1 if lowercase
+ * Return: 1 (if positive), 0 (if zero), -1 (if negative)
  */
 
-int _islower(int c)
+int print_sign(int n)
 {
-	int c;
-
-	if (islower(c))
+	if (n > 0)
 	{
+		_putchar('+');
 		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
 	}
 	else
 	{
+		_putchar(0);
 		return (0);
 	}
 }
