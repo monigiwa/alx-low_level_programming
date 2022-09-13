4685984120485415bd8860310c83172fd3970a5b
Giwa <giwatitilope899@gmail.com>
2022-09-13 16:46:46 -0700

    task 10&11

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - add two numbers from input
+ * @a: first parameter
+ * @b: second parameter
+ *
+ * Description: add two numbers
+ *
+ * Return: Always (0)
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
