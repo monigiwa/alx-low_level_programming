eafbe8cf07ee1aab2a88fc94081fc38bd8d5325f
Giwa <giwatitilope899@gmail.com>
2022-09-13 08:40:12 -0700

    task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print alphabet 10 times
+ *
+ * Return: void
+ */
+
+void print_alphabet_x10(void)
+{
+	int round = 1;
+	char ch;
+
+	for (round = 1; round <= 10; round++)
+	{
+		for (ch = 'a'; ch <= 'z'; ch++)
+		{
+			_putchar(ch);
+		}
+		_putchar('\n')
+	}
+}
