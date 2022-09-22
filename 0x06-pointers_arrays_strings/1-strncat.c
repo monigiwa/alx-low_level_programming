84f788369ff2fb237588ba3834f83a6c2a2afd57
Giwa <giwatitilope899@gmail.com>
2022-09-22 12:54:12 -0700

    strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -10,12 +10,21 @@
 
 char *_strncat(char *dest, char *src, int n)
 {
-	int n dest_len = strlen(dest);
-	int i;
+	int count = 0;
+	int count2 = 0;
 
-	for (i = 0 ; i < n && src[i] != '\0' ; i++)
-		dest[dest_len + i] = src[i];
-	dest[dest_len + i] = '\0';
+	while (*(dest + count) != '\0')
+	{
+		count++;
+	}
 
+	while (count2 < 0)
+	{
+		*(dest + count) = *(src + count2);
+		if (*(src + count2) == '\0')
+			break;
+		count++;
+		count2++;
+	}
 	return (dest);
 }
