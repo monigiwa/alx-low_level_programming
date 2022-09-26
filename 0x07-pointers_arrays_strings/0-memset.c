13b6d28ecfa68b70f2d8922778be231a10bf9a75
Giwa <giwatitilope899@gmail.com>
2022-09-26 06:18:19 -0700

    memset

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -5,10 +5,10 @@
  * @s: memory area
  * @b: constant byte
  * @n: bytes filled
- * Return: the pointer to the memory area s
+ * Return: the pointer to dest
  */
 
-char *_memset(char *s, char b, unsigned int n);
+char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
 
