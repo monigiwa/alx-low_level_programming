7fbdc8498a81f3902df5c300be7f4f274f62ea0b
Giwa <giwatitilope899@gmail.com>
2022-09-30 12:06:39 -0700

    task 4

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - a program that adds positive number
+ * @argc: the number of command line arguments
+ * @argv: an array of strings containing the program command line arguments
+ * Return: returns 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+	int sum = 0;
+	char *ptr = 0;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (0);
+	}
+	for (i = 1; i < argc; i++)
+	{
+		sum += strtol(argv[i], &ptr, 10);
+		if (*ptr != 0)
+		{
+			printf("Error\n");
+		}
+	}
+	printf("%d\n", sum);
+	return (0);
+}
