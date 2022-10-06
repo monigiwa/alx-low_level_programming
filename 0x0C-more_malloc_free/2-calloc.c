fdaf7ed2d21eb2f7782c6e855f25d6f5decc02e9
Giwa <giwatitilope899@gmail.com>
2022-10-06 13:54:53 -0700

    number of elements

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -3,23 +3,25 @@
 
 /**
  * _calloc - function that allocates memory for an array
- * @nmemb - number of elements
- * @size: size of bytes
+ * @nmemb: number of bytes.
+ * @size: size of bytes.
  *
  * Return: returns pointer to the allocated memory
+ * if nmemb or size is 0, returns NULL
+ * if malloc fails, returns NULL
  */
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	char *ptr;
 	unsigned int i;
-	
+
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 
 	ptr = malloc(nmemb * size);
 
-	if (ptr ==NULL)
+	if (ptr == NULL)
 		return (NULL);
 
 	for (i = 0; i < (nmemb * size); i++)
