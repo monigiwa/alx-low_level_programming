c039c7129f4420229dc9d1b8c989d0a590da503c
Giwa <giwatitilope899@gmail.com>
2022-10-10 12:58:24 -0700

    task 4&5

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * free_dog - a function that frees dogs
+ * @d: struct dog to free
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}
