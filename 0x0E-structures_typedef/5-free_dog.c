f068594013277478dfd77a24906a9b67d4991dd0
Giwa <giwatitilope899@gmail.com>
2022-10-10 13:47:45 -0700

    task 4&5

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,14 +1,15 @@
-#include <stdio.h>
 #include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
- * free_dog - frees memory allocated for a struct dog
- * @d: struct dog to free
+ * free_dog - free the doggos
+ * @d: doggo
  */
 
 void free_dog(dog_t *d)
 {
-	if (d)
+	if (d != NULL)
 	{
 		free(d->name);
 		free(d->owner);
