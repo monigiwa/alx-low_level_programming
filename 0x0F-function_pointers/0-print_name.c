b779a8b267f4e906a90986a1ff64e009752862f4
Giwa <giwatitilope899@gmail.com>
2022-10-12 11:57:03 -0700

    task 0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include "function_pointers.h"
+
+/**
+ * print_name - a function that prints name
+ * @name: the name to be printed
+ * @f: a pointer to a function that accepts a string as
+ * parameter and returns nothing
+ * Return: returns nothing
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL || f == NULL)
+		return;
+	f(name);
+}
