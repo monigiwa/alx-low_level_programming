be14cd6d5422e99ab539739bae21c6fa262d6cdd
Giwa <giwatitilope899@gmail.com>
2022-10-13 12:16:42 -0700

    task 3

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,92 +1,50 @@
 #include "variadic_functions.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdarg.h>
 
 /**
- * print_char - print char
- * @args: list of arguments
- */
-
-void print_char(va_list args)
-{
-	printf("%c", va_arg(args, int));
-}
-
-/**
- * print_int - print int
- * @args: list of arguments
- */
-
-void print_int(va_list args)
-{
-	printf("%d", va_arg(args, int));
-}
-
-/**
- * print_float - print float
- * @args: list of arguments
- */
-
-void print_float(va_list args)
-{
-	printf("%f", va_arg(args, double));
-}
-
-/**
- * print_string - print string
- * @args: list of arguments
- */
-
-void print_string(va_list args)
-{
-	char *s = va_arg(args, char *);
-
-	if (s == NULL)
-	{
-		printf("(nil)");
-		return;
-	}
-	printf("%s", s);
-}
-
-/**
- * print_all - prints anything*
- * @format: list of types of arguments
- */
-
+* print_all - a function that prints anything
+* @format: a list of types of arguments passed to the function
+* Return: returns nothing
+*/
 void print_all(const char * const format, ...)
 {
+	int i = 0, j = 0;
 	va_list args;
-	int i, j;
-	char *s1 = "";
-	char *s2 = ", ";
-
-	op_t ops[] = {
-		{'c', print_char},
-		{'i', print_int},
-		{'f', print_float},
-		{'s', print_string},
-		{'\0', NULL}
-	};
+	char *ptr;
 
 	va_start(args, format);
-	i = 0;
-	while (format != NULL && format[i] != '\0')
-	{
-		j = 0;
-		while (ops[j].c != '\0')
+	while (j < 1 && format != NULL)
+		while (format[i] != '\0')
 		{
-			if (ops[j].c == format[i])
+			switch (format[i])
 			{
-				printf("%s", s1);
-				ops[j].f(args);
-				s1 = s2;
+				case 'c':
+					printf("%c", va_arg(args, int));
+					break;
+				case 'i':
+					printf("%d", va_arg(args, int));
+					break;
+				case 'f':
+					printf("%f", va_arg(args, double));
+					break;
+				case 's':
+					ptr = va_arg(args, char *);
+					if (ptr == NULL)
+						ptr = "(nil)";
+					printf("%s", ptr);
+					break;
+				default:
+					i++;
+					j++;
+					continue;
 			}
 			j++;
+			if (format[i + 1] == '\0')
+			{
+				break;
+			}
+			printf(", ");
+			i++;
 		}
-		i++;
-	}
 	printf("\n");
 	va_end(args);
 }
